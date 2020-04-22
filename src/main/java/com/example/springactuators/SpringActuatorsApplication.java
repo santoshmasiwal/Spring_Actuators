e42488cf74523fffab710f725bea0aac1d434872
Santosh Masiwal <santoshmasiwal55@gmail.com>
2020-04-22 15:55:57 +0530

    Initial Commit

---
+++ src/main/java/com/example/springactuators/SpringActuatorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springactuators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springactuators/SpringActuatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springactuators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
